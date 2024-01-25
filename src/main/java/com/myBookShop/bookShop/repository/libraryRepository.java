dbffebfa2ed0274fba41066c28abe7aee9fbe98f
davodius <davidhanebrant@gmail.com>
2024-01-25 15:44:51 +0100

    second

--- src/main/java/com/myBookShop/bookShop/repository/libraryRepository.java
+++ src/main/java/com/myBookShop/bookShop/repository/libraryRepository.java
@@ -1,6 +1,7 @@
 package com.myBookShop.bookShop.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+
 import java.awt.print.Book;
 
 public interface libraryRepository extends MongoRepository<Book, String>{
