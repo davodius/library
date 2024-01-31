cd64460d306c7e43ca625860a93bd684b285cd65
davodius <davidhanebrant@gmail.com>
2024-01-31 15:39:57 +0100

    made servcies for users

---
+++ src/main/java/com/myBookShop/bookShop/services/UserService.java
@@ -0,0 +1,41 @@
+package com.myBookShop.bookShop.services;
+
+
+import com.myBookShop.bookShop.models.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    // Creating a user
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    // Getting all users
+    public List<User> getAllUsers() {
+        return libraryRepository.findAll();
+    }
+
+    // Updating a user
+    public User updateUser(User user) {
+        return userRepository.save(user);
+    }
+
+    // Getting a specific user by id
+    public User getUserById(String id) {
+        return userRepository.findById(id).get();
+    }
+
+    // Delete a specific user by using id
+    public String deleteUser(String id) {
+        userRepository.deleteById(id);
+        return "User has been terminated";
+    }
+
+}
