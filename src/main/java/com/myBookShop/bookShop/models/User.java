b90f4a168f213a59073ba8a2c14c5f514328a7fe
davodius <davidhanebrant@gmail.com>
2024-01-31 15:29:03 +0100

    made mdoel for User

---
+++ src/main/java/com/myBookShop/bookShop/models/User.java
@@ -0,0 +1,36 @@
+package com.myBookShop.bookShop.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+public class User {
+    @Id
+    private String id;
+
+    private String name;
+
+    private String email;
+
+    private String adress;
+
+    public User() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+}
