66d2415846b6f9554ee26844f6e5c3f1703ac4b0
davodius <davidhanebrant@gmail.com>
2024-02-02 09:04:15 +0100

    added repo for borrowed books

---
+++ src/main/java/com/myBookShop/bookShop/repository/BorrowedBooksRepository.java
@@ -0,0 +1,6 @@
+package com.myBookShop.bookShop.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface BorrowedBooksRepository extends MongoRepository<Loans, String> {
+}
